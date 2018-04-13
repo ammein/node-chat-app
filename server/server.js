412b70f65f634d76e2f1ed8540644de5311c8ff1
Amin Shazrin <aminshazrin@yahoo.com>
2018-04-13 17:37:21 +0800

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -10,6 +10,19 @@ var server = http.createServer(app);
 var io = socketIO(server);
 app.use(express.static(publicPath));
 
+/*
+    io.on -> Let you register event listener , we can do something with the event.
+
+    'connection' -> Is a POPULAR EVENT that we used.This lets you listen for a new connection meaning that a client connected to the server. And do something with the connection comes in.
+*/
+io.on('connection' , (socket)=>{
+    console.log("New User Connected");
+
+    socket.on('disconnect' , ()=>{
+        console.log("User was disconnected");
+    });
+});
+
 server.listen(port , ()=>{
     console.log(`Started on port ${port}`);
 });
