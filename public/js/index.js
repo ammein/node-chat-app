4fa6ad1ed74da20d75fa80c58ad8000e03134ed0
Amin Shazrin <aminshazrin@yahoo.com>
2018-04-15 18:37:12 +0800

    Add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+/// <reference path="./jquery-3.3.1.min.js" />
+
 /* 
         Initiating request from the client to the server to open up a web socket and keep the connection open.
         Critical needed in order to listen data from server AND in order to send data to the server.
@@ -19,6 +21,12 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (newMessage){
     console.log(newMessage);
+    // Make a list tag to pass in HTML
+    var li = $('<li></li>');
+    // Inject Data into list to make text only
+    li.text(`${newMessage.from} : ${newMessage.text}`);
+    // Append into messages id that can display in HTML
+    $('#messages').append(li);
 });
 
 socket.emit('createMessage' , {
@@ -27,4 +35,18 @@ socket.emit('createMessage' , {
 }, function (data){
     // acknowledgement
     console.log("Got it",data);
+});
+
+$('#message-form').on('submit' , function (e) {
+    // preventDefault is going to to prevent DEFAULT BEHAVIOUR
+    // It's default behaviour is the page refresh with the query string.
+    e.preventDefault();  
+
+
+    socket.emit('createMessage', {
+        from : 'User',
+        text : $('[name=message]').val()
+    } , function (){
+
+    });
 });
